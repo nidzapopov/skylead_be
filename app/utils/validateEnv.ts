ba70a7cb865278dd40097affea685b165d17e97d
nidzapopov <nikola8311@gmail.com>
2025-02-15 02:49:41 +0100

    env validation

--- app/utils/validateEnv.ts
+++ app/utils/validateEnv.ts
@@ -10,8 +10,12 @@ export default class ValidateEnv {
     init(): void 
     {
         cleanEnv(process.env, {
+            MONGO_PASSWORD: str(),
+            MONGO_PATH: str(),
+            MONGO_USER: str(),
             API: str(),
             PORT: port(),
+            JWT_SECRET: str(),
             NODE_ENV: str({ choices: ['development', 'test', 'production', 'staging']}),
             DATABASE: str({ choices: ['mongodb', 'sql']}),
             MONGO: str({ choices: ['local', 'cluster']}),
